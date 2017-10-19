05989dc45ee29e71a864e6264eaee1767fe8e081
Shah <chshah@bmc.com>
2017-10-18 20:23:03 -0700

    auto commit

--- src/main/java/com/spring/canvasly/services/VerificationService.java
+++ src/main/java/com/spring/canvasly/services/VerificationService.java
@@ -9,38 +9,16 @@ import java.util.Map;
 
 public class VerificationService {
 
+    public static final String signedRequestPrefix = "signed_request=";
+
     public static void verify(Request request, Response response) throws Exception {
         Map<String, String> environment = EnvironmentService.getEnvironmentMap();
-        Map<String, Object> requestBodyMap = asMap( request.body() , "UTF-8");
-        System.out.println(" request body " + request.body()  );
-        System.out.println(" requestBodyMap " + requestBodyMap );
-        String signedRequest = SecurityService.verifyAndDecodeAsJson( (String) requestBodyMap.get("signed_request"), environment.get("CANVAS_CONSUMER_SECRET") );
-        request.params().put( "signedRequest", signedRequest );
-    }
-
-
-    @SuppressWarnings("unchecked")
-    public static Map<String, Object> asMap(String urlencoded, String encoding) throws Exception {
-        Map<String, Object> map = new LinkedHashMap<>();
-        for (String keyValue : urlencoded.trim().split("&")) {
-            String[] tokens = keyValue.trim().split("=");
-            String key = tokens[0];
-            String value = tokens.length == 1 ? null : URLDecoder.decode(tokens[1], encoding);
-            String[] keys = key.split("\\.");
-            Map<String, Object> pointer = map;
-            for (int i = 0; i < keys.length - 1; i++) {
-                String currentKey = keys[i];
-                Map<String, Object> nested = (Map<String, Object>) pointer.get(keys[i]);
-                if (nested == null) {
-                    nested = new LinkedHashMap<>();
-                }
-                pointer.put(currentKey, nested);
-                pointer = nested;
-            }
-            pointer.put(keys[keys.length - 1], value);
+        if( request.body().startsWith(signedRequestPrefix) ) {
+            String signedRequestInput = request.body().substring( signedRequestPrefix.length() );
+            String decryptedSignedRequest = SecurityService.verifyAndDecodeAsJson( signedRequestInput, environment.get("CANVAS_CONSUMER_SECRET") );
+            request.params().put( "signedRequest", decryptedSignedRequest );
+        } else {
+            throw new Exception(" Signed Request not found ");
         }
-        return map;
     }
-
-
 }
