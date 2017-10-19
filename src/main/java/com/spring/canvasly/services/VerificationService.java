6a3a4fc9c5610ec77c889741346dd0f5be74899f
Shah <chshah@bmc.com>
2017-10-18 20:59:26 -0700

    auto commit

--- src/main/java/com/spring/canvasly/services/VerificationService.java
+++ src/main/java/com/spring/canvasly/services/VerificationService.java
@@ -13,25 +13,10 @@ import java.util.Map;
 
 public class VerificationService {
 
-    public static final String signedRequestPrefix = "signed_request=";
-
     public static void verify(Request request, Response response) throws Exception {
         Map<String, String> environment = EnvironmentService.getEnvironmentMap();
-        System.out.println(" request.body() " + request.body() );
-        if( request.body().startsWith(signedRequestPrefix) ) {
-            String signedRequestInput = ( request.body().substring( signedRequestPrefix.length() ) );
-
-
-            byte b[] = signedRequestInput.getBytes();
-            ByteArrayInputStream bi = new ByteArrayInputStream(b);
-            ObjectInputStream si = new ObjectInputStream(bi);
-            String[] objs = ( String[]) si.readObject();
-
-            String decryptedSignedRequest = SecurityService.verifyAndDecodeAsJson( objs[0] , environment.get("CANVAS_CONSUMER_SECRET") );
-            request.params().put( "signedRequest", StringEscapeUtils.escapeHtml(decryptedSignedRequest) );
-            System.out.println(" verification successful . signedRequest " + decryptedSignedRequest );
-        } else {
-            throw new Exception(" Signed Request not found ");
-        }
+        String decryptedSignedRequest = SecurityService.verifyAndDecodeAsJson( request.raw().getParameterMap().get("signed_request")[0] , environment.get("CANVAS_CONSUMER_SECRET") );
+        request.params().put( "signedRequest", StringEscapeUtils.escapeHtml(decryptedSignedRequest) );
+        System.out.println(" verification successful . signedRequest " + decryptedSignedRequest );
     }
 }
