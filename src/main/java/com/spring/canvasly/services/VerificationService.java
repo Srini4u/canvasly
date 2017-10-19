ef9d0faa3893578b4afd8155e460fd498e2532a0
Shah <chshah@bmc.com>
2017-10-18 20:13:45 -0700

    auto commit

--- src/main/java/com/spring/canvasly/services/VerificationService.java
+++ src/main/java/com/spring/canvasly/services/VerificationService.java
@@ -3,12 +3,42 @@ package com.spring.canvasly.services;
 import spark.Request;
 import spark.Response;
 
+import java.net.URLDecoder;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class VerificationService {
+
     public static void verify(Request request, Response response) throws Exception {
         Map<String, String> environment = EnvironmentService.getEnvironmentMap();
-        String signedRequest = SecurityService.verifyAndDecodeAsJson(request.params().get("signed_request"), environment.get("CANVAS_CONSUMER_SECRET") );
+        Map<String, Object> requestBodyMap = asMap( request.body() , "UTF-8");
+        String signedRequest = SecurityService.verifyAndDecodeAsJson( (String) requestBodyMap.get("signed_request"), environment.get("CANVAS_CONSUMER_SECRET") );
         request.params().put( "signedRequest", signedRequest );
     }
+
+
+    @SuppressWarnings("unchecked")
+    public static Map<String, Object> asMap(String urlencoded, String encoding) throws Exception {
+        Map<String, Object> map = new LinkedHashMap<>();
+        for (String keyValue : urlencoded.trim().split("&")) {
+            String[] tokens = keyValue.trim().split("=");
+            String key = tokens[0];
+            String value = tokens.length == 1 ? null : URLDecoder.decode(tokens[1], encoding);
+            String[] keys = key.split("\\.");
+            Map<String, Object> pointer = map;
+            for (int i = 0; i < keys.length - 1; i++) {
+                String currentKey = keys[i];
+                Map<String, Object> nested = (Map<String, Object>) pointer.get(keys[i]);
+                if (nested == null) {
+                    nested = new LinkedHashMap<>();
+                }
+                pointer.put(currentKey, nested);
+                pointer = nested;
+            }
+            pointer.put(keys[keys.length - 1], value);
+        }
+        return map;
+    }
+
+
 }
