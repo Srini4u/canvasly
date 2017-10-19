c0cebb76e1b7ba5b779eb62455ad5798fcddf670
Shah <chshah@bmc.com>
2017-10-18 21:02:25 -0700

    auto commit

--- src/main/java/com/spring/canvasly/services/VerificationService.java
+++ src/main/java/com/spring/canvasly/services/VerificationService.java
@@ -15,6 +15,10 @@ public class VerificationService {
 
     public static void verify(Request request, Response response) throws Exception {
         Map<String, String> environment = EnvironmentService.getEnvironmentMap();
+        String[] stsrs = request.raw().getParameterMap().get("signed_request")
+        for( int i=0; i<stsrs.length ; i++ ) {
+            System.out.println( i + "   "  + stsrs[i] );
+        }
         String decryptedSignedRequest = SecurityService.verifyAndDecodeAsJson( request.raw().getParameterMap().get("signed_request")[0] , environment.get("CANVAS_CONSUMER_SECRET") );
         request.params().put( "signedRequest", StringEscapeUtils.escapeHtml(decryptedSignedRequest) );
         System.out.println(" verification successful . signedRequest " + decryptedSignedRequest );
